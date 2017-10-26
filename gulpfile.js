435ac66751f756caee2650904f5fe222ebe9d92f
Mayko Kioschi <mayko@kioschi.com>
2017-10-25 23:02:35 -0200

    Instalação e configuração do gulp-concat.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass');
 
-gulp.task('default', function() {
-    
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function () {
+    return gulp.src('assets/src/sass/**/*.scss')
+        .pipe(concat('style.min.scss'))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('assets/css'));
 });
