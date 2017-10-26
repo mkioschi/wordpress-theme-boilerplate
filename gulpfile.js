ba0c4c49dffdd44246a8d841961647ad0366cda9
Mayko Kioschi <mayko@kioschi.com>
2017-10-25 23:33:49 -0200

    Instalação e configuração do plugin gulp-uglify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify');
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'js']);
 
 gulp.task('sass', function () {
     return gulp.src('assets/src/sass/**/*.scss')
         .pipe(concat('style.min.scss'))
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('assets/src/js/**/*.js')
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js'));
 });
