03510854b4788dc0917c153ae381bd38aeb91119
Mayko Kioschi <mayko@kioschi.com>
2017-10-26 11:50:18 -0200

    Instalação e configuração do plugin gulp-imagemin.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin');
 
-gulp.task('default', ['sass', 'js']);
+gulp.task('default', ['sass', 'js', 'img']);
 
 gulp.task('sass', function () {
     return gulp.src('assets/src/sass/**/*.scss')
@@ -17,4 +18,20 @@ gulp.task('js', function () {
         .pipe(concat('script.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('assets/js'));
+});
+
+gulp.task('img', function () {
+    gulp.src('assets/src/img/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('assets/img'));
 });
